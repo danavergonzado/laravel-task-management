969f2e7c3186d36825f0acc159b069c7916a7494
danavergonzado <me@danavergonzado.com>
2020-04-25 04:10:46 +0800

    modify home - add widgets to homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -215,7 +215,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Main</h1>
+            <h1 class="m-0 text-dark">Welcome back, Admin!</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
