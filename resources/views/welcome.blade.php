e6e1897c417505be29ef286944eeb78614aac3ea
danavergonzado <me@danavergonzado.com>
2020-04-25 04:20:48 +0800

    add widgets to dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8">
+        <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
         </div>
     </div>
 </div>
