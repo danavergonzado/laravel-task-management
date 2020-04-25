dd90c61497df73136875b9421f43f03932167c55
danavergonzado <me@danavergonzado.com>
2020-04-25 22:58:20 +0800

    create task migration

---
+++ database/migrations/2020_04_25_145204_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable();
+            $table->string('category', 100)->nullable();
+            $table->string('user_id', 5)->nullable();
+            $table->string('status', 20)->nullable();
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('finish_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
